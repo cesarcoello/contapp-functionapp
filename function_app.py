0b086eadd1c8a3c2000bb3fb9e05df27a659deb3
César Jarith Coello <jarith182@gmail.com>
2024-11-30 10:47:42 -0600

    functionapp corre local y funciona correctamente

---
+++ function_app.py
@@ -0,0 +1,14 @@
+import azure.functions as func
+import datetime
+import json
+import logging
+
+app = func.FunctionApp()
+
+@app.queue_trigger(
+        arg_name="azqueue", 
+        queue_name="activate",
+        connection="SA") 
+def queue_trigger(azqueue: func.QueueMessage):
+    logging.info('Python Queue trigger processed a message: %s',
+                azqueue.get_body().decode('utf-8'))
